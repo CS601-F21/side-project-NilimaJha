0c79d982a9e1077daf424700a622adcac4a4d3fe
Nilima Jha <njha2@dons.usfca.edu>
2021-12-15 11:16:02 -0800

    Add required html templates for the add and remove new coin.

--- app/models.py
+++ app/models.py
@@ -3,6 +3,26 @@ from datetime import datetime
 from app import db
 
 
+class CryptoCoins(db.Model):
+    coin_id = db.Column(db.String(25), primary_key=True)
+    coin_name = db.Column(db.String(30), index=True, unique=True)
+    coin_symbol = db.Column(db.String(15), index=True, unique=True)
+    coin_market_price = db.Column(db.Float(100), nullable=True)
+    tag = db.relationship('Tags', backref='associated_tags', lazy='dynamic')
+
+    def __repr__(self):
+        return '<CryptoCoins {}, {}, {}>'.format(self.coin_id, self.coin_name, self.coin_symbol)
+
+
+class Tags(db.Model):
+    tag_id = db.Column(db.Integer, primary_key=True)
+    coin_id = db.Column(db.String(25), db.ForeignKey('crypto_coins.coin_id'))
+    coin_tag = db.Column(db.String(100), index=True)
+
+    def __repr__(self):
+        return '<Tags {} : {}>'.format(self.coin_tag, self.coin_id)
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -23,22 +43,3 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
-
-class CryptoCoins(db.Model):
-    coin_id = db.Column(db.String(25), primary_key=True)
-    coin_name = db.Column(db.String(30), index=True, unique=True)
-    coin_symbol = db.Column(db.String(15), index=True, unique=True)
-    coin_market_price = db.Column(db.Float(100), nullable=True)
-    tag = db.relationship('Tags', backref='associated_tags', lazy='dynamic')
-
-    def __repr__(self):
-        return '<CryptoCoins {}, {}, {}>'.format(self.coin_id, self.coin_name, self.coin_symbol)
-
-
-class Tags(db.Model):
-    tag_id = db.Column(db.Integer, primary_key=True)
-    coin_id = db.Column(db.String(25), db.ForeignKey('crypto_coins.coin_id'))
-    coin_tag = db.Column(db.String(100), index=True)
-
-    def __repr__(self):
-        return '<Tags {} : {}>'.format(self.coin_tag, self.coin_id)
